50a32692e4e9212af4dc2a3ba2d2f4fec089bc3b
varepsilon123 <chungtszfung2@gmail.com>
2025-01-06 15:49:14 -0800

    fix: env file fix

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: '.',
-  envFile: 'env.list',
   server: {
     host: true, // Allow external connections
     port: 3000,
