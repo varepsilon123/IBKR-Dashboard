9487a12b9116facdf06ed587d86bb62e9fcdf4ed
varepsilon123 <chungtszfung2@gmail.com>
2025-01-09 10:57:04 -0800

    chore: package update and config update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +11,16 @@ export default defineConfig({
     port: 3000,
     watch: {
       usePolling: true // For Docker compatibility
-    }
+    },
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'inputs/cacert_decrypted.pem')), // Path to your PEM file
+      cert: fs.readFileSync(path.resolve(__dirname, 'inputs/cacert.pem')), // Use the same PEM file for the certificate
+    },
+    // proxy: {
+    //   '/api': {
+    //       target: 'https://localhost:5000/', // The backend server URL
+    //       changeOrigin: true, // Changes the origin of the host header to the target URL
+    //   },
+    // },
   }
 })
