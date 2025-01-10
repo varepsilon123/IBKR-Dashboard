661873f4c03f2a5ad257efa118af3f0be5803e53
varepsilon123 <chungtszfung2@gmail.com>
2025-01-09 22:50:53 -0800

    style: front page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
 import AuthCheck from './components/AuthCheck/AuthCheck'
 import Menu from './components/Menu/Menu'
 
 const App: React.FC = () => {
+  const [headerZoomed, setHeaderZoomed] = useState(true);
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [showCard, setShowCard] = useState(false);
+
+  // Effect to zoom the header after 2 second
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHeaderZoomed(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Effect to show the paragraph 1.2 second after the header is zoomed
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowParagraph(true);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, [headerZoomed]);
+
+  // Effect to show the card after the paragraph is shown
+  useEffect(() => {
+    if (showParagraph) {
+      const cardTimer = setTimeout(() => {
+        setShowCard(true);
+      }, 700);
+
+      return () => clearTimeout(cardTimer);
+    }
+  }, [showParagraph]);
+
   return (
     <div className="relative">
       <Menu />
-      <h1 className="mt-4">IBKR Dashboard</h1>
-      <p>Click the menu button on the top left corner to explore the application.</p>
-      <div className="card">
-        <AuthCheck />
-      </div>
+      <h1
+        className={`transition-all duration-1000 ease-in-out ${
+          headerZoomed
+            ? 'scale-125' : 'scale-100'
+        } mt-72 text-center animate-fadeIn`}
+      >IBKR Dashboard</h1>
+      {showParagraph && (
+        <p className="mt-2 animate-fadeIn">
+          Click the menu button on the top left corner to explore the application.
+        </p>
+      )}
+      {showCard && (
+        <div className="card animate-fadeIn">
+          <AuthCheck />
+        </div>
+      )}
     </div>
   )
 }
