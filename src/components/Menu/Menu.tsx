1c5c28ebb829dab665c859a27756946c8fc0ed86
varepsilon123 <chungtszfung2@gmail.com>
2025-01-09 22:51:07 -0800

    style: menu hovering effect

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,6 +5,8 @@ function Menu() {
     const [isOpen, setIsOpen] = useState(false);
     const [activePage, setActivePage] = useState('home');
 
+    const menuItemClasses = 'p-4 hover:bg-gray-500 transition-all hover:scale-125';
+
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
@@ -15,24 +17,26 @@ function Menu() {
     };
 
     return (
-        <div className="relative">
+        <div className="relative z-50">
             <div onClick={toggleMenu} style={{ cursor: 'pointer' }}>
                 {isOpen ? <FaTimes /> : <FaBars />}
             </div>
             {isOpen && (
                 <div className="absolute top-0 left-0 h-full w-64 mt-12">
                     <ul className="flex flex-col">
-                        <li onClick={() => handlePageChange('home')} className="p-4 hover:bg-gray-500">Home</li>
-                        <li onClick={() => handlePageChange('authCheck')} className="p-4 hover:bg-gray-500">Auth Check</li>
+                        <li onClick={() => handlePageChange('home')} className={menuItemClasses}>Home</li>
+                        <li onClick={() => handlePageChange('authCheck')} className={menuItemClasses}>Auth Check</li>
                     </ul>
                 </div>
             )}
-            {activePage === 'authCheck' && (
+
+            {/* to-do: call auth check page */}
+            {/* {activePage === 'authCheck' && (
                 <div>
                     <h2>Auth Check Placeholder Page</h2>
                     <p>This is a placeholder for the Auth Check component.</p>
                 </div>
-            )}
+            )} */}
         </div>
     );
 }
