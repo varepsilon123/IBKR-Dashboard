abdf1aaa6ea50687110e7a103cb7247996ab4918
varepsilon123 <chungtszfung2@gmail.com>
2025-01-10 21:54:14 -0800

    style: menu button style fix

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -9,7 +9,7 @@ function Menu() {
         { key: 'trend', label: 'Trend Identification', path: '/trend' },
       ];
 
-    const menuItemClasses = 'p-4 hover:bg-gray-500 transition-all hover:scale-110';
+    const menuItemClasses = 'p-4 hover:bg-gray-600 text-gray-400 hover:text-gray-300 transition-all hover:scale-110';
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -22,15 +22,18 @@ function Menu() {
             </div>
             {isOpen && (
                 <div className="animate-fadeIn absolute top-0 left-0 h-full w-64 mt-12">
+                    
                     <ul>
                         {menuItems.map((item) => (
-                          <li 
-                            key={item.key} 
-                            onClick={() => toggleMenu()} 
-                            className={menuItemClasses}
-                          >
-                            <Link to={item.path}>{item.label}</Link>
-                          </li>
+                            <Link to={item.path}>
+                                <li 
+                                  key={item.key} 
+                                  onClick={() => toggleMenu()} 
+                                  className={menuItemClasses}
+                                >
+                                  {item.label}
+                                </li>
+                            </Link>
                         ))}
                     </ul>
                 </div>
